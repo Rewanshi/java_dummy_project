b146627fcf15dd4cab54058fdfe2b07d501fbb89
Rewanshi <138444119+Rewanshi@users.noreply.github.com>
2025-05-06 17:24:41 +0530

    updated_workflow

--- dummy_project/test/MainTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class MainTest {
-    @Test
-    public void testHello() {
-        assertEquals(1, 1);
-    }
-}
--- dummy_project/test/MainTest.ts
+++
@@ -1,5 +0,0 @@
-import { greet } from '../src/main';
-
-test('greet function', () => {
-    expect(greet('World')).toBe('Hello, World!');
-});
